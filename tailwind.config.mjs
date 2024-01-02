ddc1eefa40b78eeeedaea3b52252fddeacfb8a3e
alessia vinnik <alessia.vinnik@student.howest.be>
2024-01-02 19:25:54 +0100

    first transition from page to page via Layout component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				'primary': {
+					500: '#74043B',
+				},
+				'secondary': {
+					500: '#D94B4C',
+				},
+				'tertiary': {
+					500: '#F29863',
+				},
+				'neutral': {
+					'black': '#3A3042',
+					'white': '#FEFDFE'
+				},
+			},
+		},
 	},
 	plugins: [],
 }
